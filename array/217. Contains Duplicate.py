0b5fc1a8459e79f19099fc976906bf51eb5f1491
Junying Lu <junying.lu.cn@gmail.com>
2020-08-15 00:25:39 +0800

    Update 217. Contains Duplicate.py

--- array/217. Contains Duplicate.py
+++ array/217. Contains Duplicate.py
@@ -1,3 +1,17 @@
+# Solution A
 class Solution:
     def containsDuplicate(self, nums):
         return len(nums) != len(set(nums))
+
+# Solution B
+class Solution:
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                return True
+        return False
+
+# Solution C
+class Solution:
+    def containsDuplicate(self, nums):
