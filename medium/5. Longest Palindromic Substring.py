3a9abf1a39345810aa54b934df7207a3457175a5
Junying Lu <junying.lu.cn@gmail.com>
2020-09-02 23:51:01 +0800

    Update 5. Longest Palindromic Substring.py

--- medium/5. Longest Palindromic Substring.py
+++ medium/5. Longest Palindromic Substring.py
@@ -1,3 +1,4 @@
+# Solution A
 class Solution:
     def longestPalindrome(self, s):
         size = len(s)
@@ -28,3 +29,33 @@ class Solution:
                         max_len = max_tmp
                         start = i
         return s[start:start + max_len]
+
+# Solution B
+class Solution:
+    def longestPalindrome(self, s):
+        size = len(s)
+        if size < 2:
+            return s
+
+        max_len = 1
+        res = s[0]
+
+        for i in range(size):
+            palindrome_odd, odd_len = self.center_spread(s, size, i, i)
+            palindrome_even, even_len = self.center_spread(s, size, i, i + 1)
+
+            cur_max_sub = palindrome_odd if odd_len >= even_len else palindrome_even
+            if len(cur_max_sub) > max_len:
+                max_len = len(cur_max_sub)
+                res = cur_max_sub
+
+        return res
+
+    def center_spread(self, s, size, left, right):
+        i = left
+        j = right
+
+        while i >= 0 and j < size and s[i] == s[j]:
+            i -= 1
+            j += 1
+        return s[i + 1:j], j - i - 1
