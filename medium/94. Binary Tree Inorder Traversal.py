0d5ca11dce28b65764881928651ae93cbe67a7e2
Junying Lu <junying.lu.cn@gmail.com>
2020-09-15 00:19:50 +0800

    add no.94

---
+++ medium/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        white, gery = 0, 1
+        res = []
+        stack = [(white, root)]
+        while stack:
+            color, node = stack.pop()
+            if node is None:
+                continue
+            if color == white:
+                stack.append((white, node.right))
+                stack.append((gery, node))
+                stack.append((white, node.left))
+            else:
+                res.append(node.val)
+        return res
