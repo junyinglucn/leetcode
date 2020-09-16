f7b6aa5a4b246d6a6cbe7334463c709db9b0ccf0
Junying Lu <junying.lu.cn@gmail.com>
2020-09-16 22:47:57 +0800

    add No.9

---
+++ easy/9. Palindrome Number.py
@@ -0,0 +1,19 @@
+# Solution A
+class Solution:
+    def isPalindrome(self, x):
+        x = str(x)
+        if x[::-1] == x:
+            return True
+        return False
+
+
+# Solution B
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        rem = 0
+        while x > rem:
+            rem = rem * 10 + x % 10
+            x = x // 10
+        return x == rem or x == rem // 10
