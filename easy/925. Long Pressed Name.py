5445ddb0cd6eaa9aeee72ef4890b45eb38f6f9dc
Junying Lu <junying.lu.cn@gmail.com>
2020-10-21 23:58:51 +0800

    add No.925

--- easy/925. Long Pressed Name.py
+++ easy/925. Long Pressed Name.py
@@ -1,3 +1,4 @@
+# Solution A
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
         i = 0
@@ -5,6 +6,20 @@ class Solution:
         for s in typed:
             if s == name[i]:
                 i += 1
-            elif s != name[i-1]:
+            elif s != name[i - 1]:
                 return False
         return i == len(name) - 1
+
+
+# Solution B
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        i, j = 0, 0
+        m, n = len(name), len(typed)
+        while j < n:
+            if i < m and name[i] == typed[j]:
+                i += 1
+            elif j == 0 or typed[j - 1] != typed[j]:
+                return False
+            j += 1
+        return i == m
