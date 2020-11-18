5b6e30b452ec6935faaa2c217272dcb7a428a674
Junying Lu <junying.lu.cn@gmail.com>
2020-11-19 00:07:30 +0800

    add No.14

--- easy/14. Longest Common Prefix.py
+++ easy/14. Longest Common Prefix.py
@@ -1,3 +1,4 @@
+# Solution A
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if len(strs) == 0:
@@ -8,3 +9,16 @@ class Solution:
                 if len(strs[j]) == i or strs[j][i] != tmp:
                     return strs[0][:i]
         return strs[0]
+
+
+# Solution B
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        s1 = min(strs)
+        s2 = max(strs)
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                return s1[:i]
+        return s1
