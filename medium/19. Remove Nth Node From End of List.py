e1ffa85b9a03d1d20efd512185f6ade69edaa4bb
Junying Lu <junying.lu.cn@gmail.com>
2020-11-24 00:09:30 +0800

    add No.19

--- medium/19. Remove Nth Node From End of List.py
+++ medium/19. Remove Nth Node From End of List.py
@@ -19,3 +19,29 @@ class Solution:
             cur = cur.next
         cur.next = cur.next.next
         return dummy.next
+
+
+# Solution B
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        fast, slow = dummy, dummy
+        for _ in range(n):
+            fast = fast.next
+        while fast and fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
+
+
+# Solution C
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if not head:
+            self.count = 0
+            return head
+        head.next = self.removeNthFromEnd(head.next, n)
+        self.count += 1
+        return head.next if self.count == n else head
