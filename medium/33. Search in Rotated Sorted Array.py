7571ec41018255ed7d8bae06cfe4bb590c9c944b
Junying Lu <junying.lu.cn@gmail.com>
2020-12-08 00:01:56 +0800

    No.33

--- medium/33. Search in Rotated Sorted Array.py
+++ medium/33. Search in Rotated Sorted Array.py
@@ -5,3 +5,27 @@ class Solution:
             if nums[i] == target:
                 return i
         return -1
+
+
+# Solution B
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[left]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid
+                else:
+                    right = mid - 1
+        return -1
