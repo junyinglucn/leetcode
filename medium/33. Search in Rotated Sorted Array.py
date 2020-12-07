ff91b8c4163f9853ecaf94dcb3c506a165e0b349
Junying Lu <junying.lu.cn@gmail.com>
2020-12-07 23:46:45 +0800

    No.33

---
+++ medium/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,7 @@
+# Solution A
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
