c812967100a18ed8239acd68241bd0ccce408847
Junying Lu <junying.lu.cn@gmail.com>
2021-01-22 01:07:27 +0800

    No.49

--- medium/49. Group Anagrams.py
+++ medium/49. Group Anagrams.py
@@ -1,3 +1,4 @@
+# Solution A
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         mp = collections.defaultdict(list)
@@ -5,4 +6,16 @@ class Solution:
             key = ''.join(sorted(st))
             mp[key].append(st)
 
+        return list(mp.values())
+
+
+# Solution B
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = collections.defaultdict(list)
+        for st in strs:
+            ct = [0] * 26
+            for ch in st:
+                ct[ord(ch) - ord('a')] += 1
+            mp[tuple(ct)].append(st)
         return list(mp.values())
