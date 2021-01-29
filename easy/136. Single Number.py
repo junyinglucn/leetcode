a24e07f3542211246f136787970854658ebd2903
Junying Lu <junying.lu.cn@gmail.com>
2021-01-30 00:34:08 +0800

    add No.136

--- easy/136. Single Number.py
+++ easy/136. Single Number.py
@@ -1,2 +1,21 @@
+# Solution A
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if i == len(nums) - 1 and nums[i - 1] == nums[i]:
+                continue
+            elif i != len(nums) - 1 and (nums[i - 1] == nums[i] or nums[i + 1] == nums[i]):
+                continue
+            else:
+                return nums[i]
+        return nums[0]
+
+
+# Solution B
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
