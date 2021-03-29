e8eba08acc3eb345624f5343bba10a64c24f8f93
Junying Lu <junying.lu.cn@gmail.com>
2021-03-29 22:29:46 +0800

    add JZ No.11

--- 剑指/剑指 Offer 11. 旋转数组的最小数字.py
+++ 剑指/剑指 Offer 11. 旋转数组的最小数字.py
@@ -1,3 +1,4 @@
+# Solution A:
 class Solution:
     def minArray(self, numbers: List[int]) -> int:
         n = len(numbers)
@@ -5,3 +6,19 @@ class Solution:
             if numbers[i - 1] > numbers[i]:
                 return numbers[i]
         return numbers[0]
+
+
+# Solution B
+class Solution:
+    def minArray(self, numbers: List[int]) -> int:
+        low = 0
+        high = len(numbers) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if numbers[mid] < numbers[high]:
+                high = mid
+            elif numbers[mid] > numbers[high]:
+                low = mid + 1
+            else:
+                high -= 1
+        return numbers[low]
